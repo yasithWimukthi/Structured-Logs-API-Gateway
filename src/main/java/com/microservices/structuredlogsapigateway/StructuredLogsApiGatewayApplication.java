b2f494104b67b3ce97601d2bfb541e818e22e5b2
yasithwimukthi <wimukthiyasith@gmail.com>
2026-05-01 21:34:20 +0530

    Initial commit

---
+++ src/main/java/com/microservices/structuredlogsapigateway/StructuredLogsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.structuredlogsapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuredLogsApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StructuredLogsApiGatewayApplication.class, args);
+    }
+
+}
